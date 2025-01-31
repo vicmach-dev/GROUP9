f45f0e0c0bd124b7f6a031843567f0e9d3f07791
chiedoziefavour <chiedoziefavour80@gmail.com>
2025-01-31 14:10:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Home from './Component/Home'
+import { FaFacebook } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa6";
+import { RiPhoneFill } from "react-icons/ri";
+import { FaEnvelope } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
+import { RiStarFill } from "react-icons/ri";
+
+const App = () => {
+  
+
+  return (
+    <>
+      <Home/>    
+    </>
+  )
+}
+
+export default App

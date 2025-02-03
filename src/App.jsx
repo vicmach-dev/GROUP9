6b304721be6b77af751f104b19061cebfbafc631
chiedoziefavour <chiedoziefavour80@gmail.com>
2025-02-03 13:49:28 +0100

    Massive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Home from './Component/Home'
 import { FaFacebook } from "react-icons/fa";
 import { CiLinkedin } from "react-icons/ci";
-import { FaTwitter } from "react-icons/fa";
+import { RiTwitterXLine } from "react-icons/ri";
 import { FaInstagram } from "react-icons/fa";
 import { FaRegCopyright } from "react-icons/fa6";
 import { RiPhoneFill } from "react-icons/ri";
